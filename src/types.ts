e468cf66df7318f63175a058cd1b6cbf681f5732
Laura Ferrando Ferrero <lauraferrandof@gmail.com>
2023-08-15 21:31:29 +0200

    feat: update site config

--- src/types.ts
+++ src/types.ts
@@ -19,24 +19,4 @@ export type SocialIcons = {
   [social in SocialMedia]: string;
 };
 
-export type SocialMedia =
-  | "Github"
-  | "Facebook"
-  | "Instagram"
-  | "LinkedIn"
-  | "Mail"
-  | "Twitter"
-  | "Twitch"
-  | "YouTube"
-  | "WhatsApp"
-  | "Snapchat"
-  | "Pinterest"
-  | "TikTok"
-  | "CodePen"
-  | "Discord"
-  | "GitLab"
-  | "Reddit"
-  | "Skype"
-  | "Steam"
-  | "Telegram"
-  | "Mastodon";
+export type SocialMedia = "Github" | "LinkedIn" | "Mail";

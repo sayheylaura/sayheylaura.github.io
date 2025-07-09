5ae9abe57b2eca70a34ea4f6d0230350541f3f19
sayheylaura <lauraferrandof@gmail.com>
2025-07-10 00:43:30 +0200

    chore: fix deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sayheylaura.github.io/',
+  base: '/sayheylaura.github.io/',
   integrations: [
     tailwind({
       config: {
